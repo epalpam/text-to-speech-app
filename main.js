e44f145a8a60ad54e470953c3afe02fc5a47cea3
Paminos <109203882+epalpam@users.noreply.github.com>
2022-08-12 16:42:53 +0300

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+
+document.querySelector('button').addEventListener('click', () => {
+    VoiceRSS.speech( {
+        key: '655c696f6e34430d85f491206e9bef2b',
+        src: 'Χαρουλι πρασινοματικο μωρουλι, Χαρουλι απαλουλι, Χαρουλι τρυφερουλι, Χαρουλι ζαβουλι, Χαρουλι καυλακι',
+        hl: 'el-gr',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+});
+
+
---
+++ notes.md
@@ -0,0 +1,14 @@
+
+## Open issues
+
+#### Github & Github desktop setup
+#### favicon
+#### responsive design, check
+#### make it full stack....mongo db etc
+#### live site, heroku etc
+
+## Useful links
+
+### https://dribbble.com/, for the gif, explore the world’s leading design portfolios
+### https://www.voicerss.org/, text-to-speech api
+### https://rapidapi.com/hub, discover and connect to thousands of APIs
